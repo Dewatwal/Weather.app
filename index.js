d7f9aa0704f2f559dd8eab0822b1dd82ee1f87bc
Deepak Dewatwal <136556140+Dewatwal@users.noreply.github.com>
2025-04-03 13:01:24 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const city = document.querySelector(".search-input");
 const btn = document.querySelector(".search-btn");
-
+const weatherEmojis = {
+  Thunderstorm: "⛈️",
+  Drizzle: "🌧️",
+  Rain: "☔",
+  Snow: "❄️",
+  Mist: "🌫️",
+  Smoke: "💨",
+  Haze: "🌁",
+  Dust: "🏜️",
+  Fog: "🌫️",
+  Sand: "🏜️",
+  Ash: "🌋",
+  Squall: "🌬️",
+  Tornado: "🌪️",
+  Clear: "☀️",
+  Clouds: "☁️"
+};
 const apikey = '3ceb42c9b928f51fbb49487ef14e88b9';
 btn.addEventListener("click",()=>{
   const cityInput = city.value.trim();
@@ -12,14 +28,19 @@ btn.addEventListener("click",()=>{
     const now = new Date();
     document.querySelector(".city").innerHTML =`${cityInput}`;
     document.querySelector(".date").innerHTML =`${now.toDateString()}`;
-    document.querySelector(".description").innerHTML = `${data.weather[0].main}`;
-    document.querySelector(".temp").innerHTML = `${data.main.temp}`;
-    // document.querySelector(".detail-value").innerHTML = `${};`
-    
+    const weatherType = data.weather[0].main;
+    const emoji = weatherEmojis[weatherType];
+    document.querySelector(".weather-icon").innerHTML= emoji;
+    const temp = data.main.temp;
+    document.querySelector(".temp").innerHTML = `${Math.trunc(temp)}`;
+    document.querySelector(".description").innerHTML = `${weatherType}`;
+    const detail = document.querySelectorAll(".detail-value");   
+      detail[0].innerHTML = `${data.main.humidity }%`;
+      detail[1].innerHTML = `${data.wind.speed} km/hr`;
+      detail[2].innerHTML = `${data.main.pressure} hPa`;
   }).catch(err=>{
     console.log(err);
   })
-
 });
 
 
